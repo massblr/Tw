d37b7761f8158d9706a9b9e0c7e491cc28889990
massblr <massblri@gmail.com>
2020-04-15 15:27:19 +0200

    o

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -3,11 +3,22 @@ require 'pry'
 require 'twitter'
 Dotenv.load('.env')
 
+def login_twitter
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
 end
+end
+def messages
+  client.update('Mon premier tweet Ruby !!')
+  client.update('coucou !!')
+  client.update('coucou mona!!')
+end
 
-client.update('Mon premier tweet Ruby !!')
+def perform
+  login_twitter
+  messages
+end
+perform
